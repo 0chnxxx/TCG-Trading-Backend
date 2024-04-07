425a3b2e5edecc45a5f7b77a6066899c84147c6b
chan <0chn.xxx@gmail.com>
2024-04-07 13:59:16 +0900

    :white_check_mark: Add Swagger Security Scheme

--- api/src/main/kotlin/com/trading/tcg/global/config/SwaggerConfig.kt
+++ api/src/main/kotlin/com/trading/tcg/global/config/SwaggerConfig.kt
@@ -3,6 +3,8 @@ package com.trading.tcg.global.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,7 +15,11 @@ class SwaggerConfig {
     fun openApi(): OpenAPI {
         return OpenAPI()
             .servers(listOf(Server().url("http://localhost:8080").description("로컬 서버")))
-            .components(Components())
+            .components(
+                Components()
+                    .addSecuritySchemes("Authorization", SecurityScheme().type(SecurityScheme.Type.APIKEY).name("Authorization").`in`(SecurityScheme.In.HEADER))
+            )
+            .security(listOf(SecurityRequirement().addList("Authorization")))
             .info(
                 Info()
                     .title("TCG-Trading API")
